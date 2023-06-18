cdc1d48865c6d6e0ffecc29146c3966b85ac0665
inventor97 <mr.inventor97@gmail.com>
2023-06-19 03:41:48 +0500

    user repositories integrated

---
+++ lib/models/user_details_model.dart
@@ -0,0 +1,143 @@
+class UserTodosModel {
+  final int? userId;
+  final int? id;
+  final String? title;
+  final bool? completed;
+
+  UserTodosModel({
+    this.userId,
+    this.id,
+    this.title,
+    this.completed,
+  });
+
+  factory UserTodosModel.fromJson(Map<String, dynamic> json) => UserTodosModel(
+    userId: json["userId"],
+    id: json["id"],
+    title: json["title"],
+    completed: json["completed"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "userId": userId,
+    "id": id,
+    "title": title,
+    "completed": completed,
+  };
+}
+
+class UserAlbumsModel {
+  final int? userId;
+  final int? id;
+  final String? title;
+
+  UserAlbumsModel({
+    this.userId,
+    this.id,
+    this.title,
+  });
+
+  factory UserAlbumsModel.fromJson(Map<String, dynamic> json) => UserAlbumsModel(
+    userId: json["userId"],
+    id: json["id"],
+    title: json["title"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "userId": userId,
+    "id": id,
+    "title": title,
+  };
+}
+
+class UserPostsModel {
+  final int? userId;
+  final int? id;
+  final String? title;
+  final String? body;
+
+  UserPostsModel({
+    this.userId,
+    this.id,
+    this.title,
+    this.body,
+  });
+
+  factory UserPostsModel.fromJson(Map<String, dynamic> json) => UserPostsModel(
+    userId: json["userId"],
+    id: json["id"],
+    title: json["title"],
+    body: json["body"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "userId": userId,
+    "id": id,
+    "title": title,
+    "body": body,
+  };
+}
+
+class PhotosModel {
+  final int? albumId;
+  final int? id;
+  final String? title;
+  final String? url;
+  final String? thumbnailUrl;
+
+  PhotosModel({
+    this.albumId,
+    this.id,
+    this.title,
+    this.url,
+    this.thumbnailUrl,
+  });
+
+  factory PhotosModel.fromJson(Map<String, dynamic> json) => PhotosModel(
+    albumId: json["albumId"],
+    id: json["id"],
+    title: json["title"],
+    url: json["url"],
+    thumbnailUrl: json["thumbnailUrl"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "albumId": albumId,
+    "id": id,
+    "title": title,
+    "url": url,
+    "thumbnailUrl": thumbnailUrl,
+  };
+}
+
+class CommentsModel {
+  final int? postId;
+  final int? id;
+  final String? name;
+  final String? email;
+  final String? body;
+
+  CommentsModel({
+    this.postId,
+    this.id,
+    this.name,
+    this.email,
+    this.body,
+  });
+
+  factory CommentsModel.fromJson(Map<String, dynamic> json) => CommentsModel(
+    postId: json["postId"],
+    id: json["id"],
+    name: json["name"],
+    email: json["email"],
+    body: json["body"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "postId": postId,
+    "id": id,
+    "name": name,
+    "email": email,
+    "body": body,
+  };
+}
--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,18 +1,70 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:test_app_talatix/models/user_details_model.dart';
 import 'package:test_app_talatix/models/user_info_model.dart';
 import 'package:test_app_talatix/repositories/_base_repo.dart';
 
 class UserInfoRepository extends BaseRepository {
-  
   Future<List<UserInfoModel>> getUsers() async {
     Response response = await makeHttpRequest("users", {});
 
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       final parsed = await jsonDecode(utf8.decode(response.bodyBytes));
       return List<UserInfoModel>.from(parsed?.map((e) => UserInfoModel.fromJson(e)) ?? []);
     }
-    throw("Error");
+    throw ("Error");
+  }
+
+  Future<List<UserAlbumsModel>> getUserAlbums(int userId) async {
+    Response response = await makeHttpRequest("albums?userId=$userId", {});
+
+    if (response.statusCode == 200) {
+      final parsed = await jsonDecode(utf8.decode(response.bodyBytes));
+      return List<UserAlbumsModel>.from(parsed?.map((e) => UserAlbumsModel.fromJson(e)) ?? []);
+    }
+    throw ("Error");
+  }
+
+  Future<List<UserPostsModel>> getUserPosts(int userId) async {
+    Response response = await makeHttpRequest("posts?userId=$userId", {});
+
+    if (response.statusCode == 200) {
+      final parsed = await jsonDecode(utf8.decode(response.bodyBytes));
+      return List<UserPostsModel>.from(parsed?.map((e) => UserPostsModel.fromJson(e)) ?? []);
+    }
+
+    throw ("Error");
+  }
+
+  Future<List<UserTodosModel>> getUserTodos(int userId) async {
+    Response response = await makeHttpRequest("todos?userId=$userId", {});
+
+    if (response.statusCode == 200) {
+      final parsed = await jsonDecode(utf8.decode(response.bodyBytes));
+      return List<UserTodosModel>.from(parsed?.map((e) => UserTodosModel.fromJson(e)) ?? []);
+    }
+
+    throw ("Error");
+  }
+
+  Future<List<PhotosModel>> getPhotos(int albumId) async {
+    Response response = await makeHttpRequest("phptos?albumId=$albumId", {});
+
+    if (response.statusCode == 200) {
+      final parsed = await jsonDecode(utf8.decode(response.bodyBytes));
+      return List<PhotosModel>.from(parsed?.map((e) => PhotosModel.fromJson(e)) ?? []);
+    }
+    throw ("Error");
+  }
+
+  Future<List<CommentsModel>> getComments(int postId) async {
+    Response response = await makeHttpRequest("comments?postId=$postId", {});
+
+    if (response.statusCode == 200) {
+      final parsed = await jsonDecode(utf8.decode(response.bodyBytes));
+      return List<CommentsModel>.from(parsed?.map((e) => CommentsModel.fromJson(e)) ?? []);
+    }
+    throw ("Error");
   }
 }
